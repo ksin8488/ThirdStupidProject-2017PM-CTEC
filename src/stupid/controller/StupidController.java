bb28adae3146d2e70db742c922e0f03f95ecdd82
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-06 12:55:34 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,9 @@
+package stupid.controller;
+
+public class StupidController
+{
+	public void start()
+	{
+		System.out.println("I will program well. ");
+	}
+}
---
+++ src/stupid/controller/StupidRunner.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidRunner
+{
+	public static void main(String [] args)
+	{
+		StupidController app = new StupidController();
+		app.start();
+	}
+}
